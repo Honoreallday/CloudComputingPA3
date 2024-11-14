470fe67b5e10a696a1d2d8511e37e7d724015e12
samshefnie <samshefnie@gmail.com>
2024-11-13 23:34:28 -0600

    initial commit

---
+++ iotProducer.py
@@ -0,0 +1,48 @@
+from keras.datasets import cifar10
+import random
+import json
+from kafka import KafkaProducer
+import cv2
+import uuid
+import time
+
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+producer = KafkaProducer(bootstrap_servers='192.168.5.236:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+label_map = {
+    0: 'airplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck'
+}
+
+def add_noise(image):
+    return cv2.GaussianBlur(image, (5, 5), 0)
+
+def send_image_to_kafka():
+    idx = random.randint(0, len(test_images) - 1)
+    image = test_images[idx]
+    noisy_image = add_noise(image)
+    ground_truth = train_labels[idx][0]
+    ground_truth_str = label_map[ground_truth]
+    
+    data = {
+        'ID': str(uuid.uuid4()),
+        'GroundTruth': ground_truth_str,
+        'Data': noisy_image.tolist()
+    }
+    
+    producer.send('image-topic', value=data)
+    print(f"Sent image ID {data['ID']} to Kafka")
+
+while True:
+    send_image_to_kafka()
+    time.sleep(1)
